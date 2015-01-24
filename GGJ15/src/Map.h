078cb995d9e02f0328dd1f1f7f4aa2c66d2cf4b0
SeanSoraghan <sean@roli.com>
2015-01-24 12:35:47 +0000

    map

--- GGJ15/src/Map.h
+++ GGJ15/src/Map.h
@@ -28,6 +28,15 @@ public:
     {
         m_shader = gl::GlslProg (loadResource (BIRD_VERT), loadResource (BIRD_FRAG));
         m_grid = new int[width*height];
+		m_cellWidth = getWindowWidth() / m_width;
+		m_cellHeight = getWindowHeight() / m_height;
+		for (int i = 0; i < m_width; i++)
+		{
+			for (int j = 0; j < m_height; j++)
+			{
+				m_grid[j+m_width + i] = 0;
+			}
+		}
     };
     
     void setState(int x, int y, int state)
@@ -47,9 +56,25 @@ public:
         gl::setMatricesWindow( getWindowSize() );
 
         gl::drawSolidRect( getWindowBounds() );
+//		for (int i = 0; i < m_width; i++)
+//		{
+//			for (int j = 0; j < m_height; j++)
+//			{
+//				if (m_grid[j*m_width + i] == 0)
+//				{
+//					gl::color( 1 , 0 , 0 );
+//					gl::begin (GL_QUADS);
+//					gl::vertex (Vec2f ((i + 1) * m_cellWidth, (j + 1) * m_cellHeight));
+//					gl::vertex (Vec2f ((i + 1) * m_cellWidth, j * m_cellHeight));
+//					gl::vertex (Vec2f (i * m_cellWidth, j * m_cellHeight));
+//					gl::vertex (Vec2f (i * m_cellWidth, (j + 1) * m_cellHeight));
+//					gl::end();
+//				}
+//			}
+//		}
     }
 private:
-    int m_width, m_height;
+    int m_width, m_height, m_cellWidth, m_cellHeight;
     int *m_grid;
     gl::GlslProg m_shader;
 };
